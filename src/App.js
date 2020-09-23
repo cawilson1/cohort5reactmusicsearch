6ceadc7c688f0c3a919c46dda9493aac2b0c2d44
cawilson1 <cawilson1@g.cofc.edu>
2020-09-23 10:11:02 -0400

    make api call to itunes api and render rsults to the page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return <div>page</div>;
+  const [songs, setSongs] = useState([]);
+  useEffect(() => {
+    async function fetchSongs() {
+      try {
+        const result = await axios.get(
+          "https://itunes.apple.com/search?term=beatles"
+        );
+        const songsArray = result.data.results;
+        console.log(songsArray);
+        setSongs(songsArray);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchSongs();
+
+    // axios
+    //   .get("https://itunes.apple.com/search?term=beatles")
+    //   .then(response => {
+    //     console.log(response);
+    //     console.log(response.data);
+    //     console.log(response.data.results);
+    //     setSongs(response.data.results);
+    //   })
+    //   .catch(err => console.log(err));
+  }, []);
+
+  return (
+    <div>
+      {songs.map((song, index) => (
+        <div key={index}>{song.collectionName}</div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
